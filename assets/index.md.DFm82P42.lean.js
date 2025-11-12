1c5ef310fb7ea65598b4d853c20586040272a5e2
R_K <126707002+kodeforgeX@users.noreply.github.com>
2025-11-12 05:58:26 +0000

    Updates

---
+++ assets/index.md.DFm82P42.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o,ae as r}from"./chunks/framework.B_owRgTZ.js";const h=JSON.parse('{"title":"Authjoy","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),n={name:"index.md"};function i(s,t,l,d,c,u){return o(),a("div",null,[...t[0]||(t[0]=[r("",6)])])}const _=e(n,[["render",i]]);export{h as __pageData,_ as default};
